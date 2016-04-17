4cad3a02fe2f6f11650a5626f610c9d5f4702945
Christian Blas Acosta <cristian12dn@gmail.com>
2016-04-17 14:06:25 -0500

    nose

---
+++ clase01/Demo005/src/pe/egcc/service/MateService.java
@@ -0,0 +1,20 @@
+package pe.egcc.service;
+
+public class MateService {
+
+
+	  public int sumar(int n1, int n2) {
+		  System.out.println("GG1");
+	    return (n1 + n2);
+	  }
+
+	  public int restar(int n1, int n2) {
+		  System.out.println("GG2");
+	    return (n1 - n2);
+	  }
+
+	  public int multiplicar(int n1, int n2) {
+		  System.out.println("GG3");
+	    return (n1 * n2);
+	  }
+}
